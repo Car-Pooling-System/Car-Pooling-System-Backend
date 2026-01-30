82309d6214c47014e514d3327c844d180fb0f4d7
Kuladeep-Reddy-C <ckuladeepreddy2006@gmail.com>
2026-01-30 11:43:35 +0530

    Database Connection

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from "./config/db.js";
 
 import healthRouter from './routes/health.router.js';
 
 dotenv.config()
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,5 +16,5 @@ app.use(cors());
 app.use('/health', healthRouter);
 
 app.listen(PORT, () => {
-    console.log("server is running on http://0.0.0.0:" + PORT);
+    console.log(`server is running on http://0.0.0.0:${PORT}`);
 })
