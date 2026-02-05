5c3f4e0580a3c3b5f4b35f26d48af4f0f51a0a47
Harish6446 <167738136+Harish6446@users.noreply.github.com>
2026-02-06 02:03:39 +0530

    Added the carbon emission database schema

---
+++ models/carbonEmission.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const emissionSchema = new mongoose.Schema({
+
+    type: {
+      type: String,
+      required: true
+    },
+
+    emissionFactor: {
+      type: Number,
+      required: true
+    }
+});
+
+export default mongoose.model("Emission", emissionSchema);
