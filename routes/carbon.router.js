a608ded71721c91381bd3a33430f8276114b5dd6
Harish6446 <167738136+Harish6446@users.noreply.github.com>
2026-02-06 12:17:10 +0530

    Update carbon.router.js

--- routes/carbon.router.js
+++ routes/carbon.router.js
@@ -10,27 +10,37 @@ router.get('/', async (req, res) => {
         const Car = await Emission.findOne({ type: type });
         const emissionFactor = Car.emissionFactor;
 
-        const driverDist = Math.max(distances);
+        const maxD = Math.max(...distances);
+        const summationD = distances.reduce((acc, current) => acc + current, 0);
 
-        const emissionArr = [];
+        var emissionArr = [];
+        var realEmission = [];
         var totalEmission = 0;
-        var driverEmission = 0;
         for (let i = 0; i < distances.length; i++){
-            if (distances[i] === driverDist){
-                var emission = emissionFactor * distances[i];
-                emissionArr.push(emission);
-                driverEmission += emission;
-                totalEmission += emission;
-            } else {
-                var emission = emissionFactor * distances[i];
-                emissionArr.push(emission);
-                totalEmission += emission;
-            }
+            var emission = (emissionFactor * distances[i]) / 1000;
+            emission = Number(emission.toFixed(4));
+            emissionArr.push(emission);
+            totalEmission += emission;
+            var real = emission * (distances[i] / summationD);
+            realEmission.push(Number(real.toFixed(4)));
         }
+        totalEmission = Number(totalEmission.toFixed(4));
 
-        const savedEmission = totalEmission - driverEmission;
+        var emissionCaused = Math.max(...emissionArr);
+        emissionCaused = Number(emissionCaused.toFixed(4));
+        var savedEmission = totalEmission - emissionCaused;
+        savedEmission = Number(savedEmission.toFixed(4));
         
-        res.status(200).json({message: `Emission in grams is: ${emissionArr}\nEmission if seperate: ${totalEmission}\nEmission saved: ${savedEmission}`});
+        res.status(200).json({ 
+            message: "Emission per passenger if separate, Total Emission if seperate, Total Emission caused, Emission Saved, Real per capita emission in kilograms", 
+            body: {
+                emissionArr, 
+                totalEmission, 
+                emissionCaused, 
+                savedEmission, 
+                realEmission
+            }
+        });
     } catch (err){
         res.status(300).json({ message: "Server error", error: err.message });
     }
